c7c8f097f9dbff6c50db881b97903a6daf2f9280
LaneGoolsby <lanegoolsby@yahoo.com>
2016-07-07 19:57:27 -0400

    Tutorial 4

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import {OnInit} from '@angular/core';
 
 @Component({
     selector: 'my-app',
@@ -66,27 +68,22 @@ import { HeroDetailComponent } from './hero-detail.component';
             }
             `],
     directives : [HeroDetailComponent],
+    providers: [HeroService]    
 })
 
-export class AppComponent { 
+export class AppComponent implements OnInit{ 
     title = "Tour of Heroes";
-    heroes = HEROES;
+    heroes : Hero[];
     selectedHero : Hero;
+
     onSelect(hero:Hero){
         this.selectedHero = hero;
     }
+
+    constructor(private heroService : HeroService){}
+    ngOnInit(){
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
 }
 
-const HEROES : Hero[] = [
-    {id: 11, name: "Mr Nice"},
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
 
