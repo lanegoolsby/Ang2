596f0a19c25290c051fe1d869c389237013cd34a
LaneGoolsby <lanegoolsby@yahoo.com>
2016-07-08 09:11:54 -0400

    Tutorial 5

---
+++ app/heroes.component.ts
@@ -0,0 +1,42 @@
+import {Component} from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import {OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+    selector: 'my-heroes',
+    templateUrl: 'app/heroes.component.html',
+    styleUrls: ['app/heroes.component.css'],
+    directives : [HeroDetailComponent],
+    providers: []    
+})
+
+export class HeroesComponent implements OnInit{ 
+    heroes : Hero[];
+    selectedHero : Hero;
+
+    constructor(
+        private router: Router,
+        private heroService : HeroService
+    ){}
+
+    getHeroes(){
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    onSelect(hero:Hero){
+        this.selectedHero = hero;
+    }
+
+    ngOnInit(){
+        this.getHeroes();
+    }
+
+    gotoDetail(){
+        this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+}
+
+
